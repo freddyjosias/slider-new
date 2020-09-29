7e7b13986a07b0fd03fe3f709cca67df053a9f3b
Freddy Josias Herrera Becerra <gerson123tuesta@gmail.com>
2020-09-28 19:14:01 -0500

    Cambios en el slider

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ $(function() {
     'use strict';
 
     $('.contenido').hide();
+    $('.contenido').css('opacity','0');
 
     var numAMostrar = 4;
     var bloques = 0;
@@ -10,31 +11,34 @@ $(function() {
     console.log($('.contenido').length);
 
     for (let index = 0; index < numAMostrar; index++) {
-        $('.contenido:nth(' + index + ')').show();
+        $('.contenido:nth(' + index + ')').show().animate({opacity:'1'});
     }
     
     bloques = 1;
     $('.next').click(function() {
-        $('.contenido').fadeOut(500);
+        $('.contenido').animate({opacity:'0'}, 300);
+        $('.contenido').hide();
         if (bloques == maxBloques) {
             bloques = 1;
         } else {
             bloques++;
         }
+        
         for (let index = (bloques - 1) * numAMostrar; index < numAMostrar * bloques; index++) {
-            $('.contenido:nth(' + index + ')').fadeIn(500);
+            $('.contenido:nth(' + index + ')').show().animate({opacity:'1'}, 300);
         }
     })
 
     $('.prev').click(function() {
-        $('.contenido').fadeOut(500);
+        $('.contenido').animate({opacity:'0'}, 300);
+        $('.contenido').hide();
         if (bloques == 1) {
             bloques = maxBloques;
         } else {
             bloques--;
         }
         for (let index = (bloques * numAMostrar) - 1; index > ((bloques - 1) * numAMostrar) - 1; index--) {
-            $('.contenido:nth(' + index + ')').fadeIn(500);
+            $('.contenido:nth(' + index + ')').show().animate({opacity:'1'}, 300);
         }
     })
 
